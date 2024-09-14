11461f1060c5767a91a39522da99e0f4d93e62b8
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-09-14 17:56:51 +0200

    adding tabs icons

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { Tabs, Redirect } from "expo-router";
+import { icons } from "../../icons/icons";
+
+const TabIcon = ({ icon, color, focused }) => {
+  return (
+    <View>
+      <Image
+        source={icon}
+        style={{
+          width: 24,
+          height: 24,
+          tintColor: focused ? color : "#5f6368",
+        }}
+        resizeMode="contain"
+      />
+    </View>
+  );
+};
 
 const TabsLayout = () => {
   return (
     <>
       <Tabs>
-        <Tabs.Screen name="home" />
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon icon={icons.home} focused={focused} />
+            ),
+          }}
+        />
       </Tabs>
     </>
   );
---
+++ icons/icons.js
@@ -0,0 +1,5 @@
+const icons = {
+  home: require("./home.png"),
+};
+
+export { icons };
