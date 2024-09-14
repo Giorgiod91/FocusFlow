f7223ae79fcfe9a6ca330ccde2b165bada288c35
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-09-14 16:37:51 +0200

    importeds fonts

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from "expo-router";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-primary">
-      <Text className="text-3xl">🚀 FocusFlow</Text>
+      <Text className="text-3xl text-pmedium text-white">🚀 FocusFlow</Text>
       <Text> Boost Your Productivity Anywhere</Text>
       <StatusBar style="auto" />
       <Button className="bg-secondary border-4" title="Login" />
